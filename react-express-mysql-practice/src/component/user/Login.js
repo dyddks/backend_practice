503934bda3f9780d7703081053583c96451c01e7
DESKTOP-KSUP4OT\dyddk <dyddks0115@naver.com>
2024-01-19 12:23:45 +0900

    feat: 회원관리 리액트컴포넌트 및 api 작성

---
+++ react-express-mysql-practice/src/component/user/Login.js
@@ -0,0 +1,48 @@
+import styled from "@emotion/styled";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 50vh;
+`;
+
+const Input = styled.input`
+  width: 50%;
+  font-size: 2rem;
+`;
+
+export const Login = () => {
+  const nav = useNavigate();
+  const [email, setEmail] = useState('');
+  const [pw, setPw] = useState('');
+
+  const register = () => {
+    axios.post('/login', {
+      email: email,
+      pw: pw
+    })
+    .then((result) => {
+      if (result.status === 200) {
+        alert(`${result.data.name}님 환영합니다.`)
+      }
+    })
+    .catch((err) => {
+      alert('잠시후 다시 시도해주세요.')
+    })
+  }
+
+  return(
+    <Container>
+      <Input type="text" placeholder="이메일" onChange={(e) => setEmail(e.target.value)}></Input>
+      <Input type="password" placeholder="비밀번호" onChange={(e) => setPw(e.target.value)}></Input>
+      <button onClick={register}>로그인</button>
+      <button onClick={() => {nav('/')}}>메인으로</button>
+    </Container>
+  )
+}
---
+++ react-express-mysql-practice/src/component/user/Register.js
@@ -0,0 +1,54 @@
+import styled from "@emotion/styled";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 50vh;
+`;
+
+const Input = styled.input`
+  width: 50%;
+  font-size: 2rem;
+`;
+
+export const Register = () => {
+  const nav = useNavigate();
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [pw, setPw] = useState('');
+
+  const register = () => {
+    axios.post('/user/register', {
+      name: name,
+      phone: phone,
+      email: email,
+      pw: pw
+    })
+    .then((result) => {
+      if (result === 1) {
+        alert('회원가입을 축하합니다.')
+      }
+    })
+    .catch((err) => {
+      alert('잠시후 다시 시도해주세요.')
+    })
+  }
+
+  return(
+    <Container>
+      <Input type="text" placeholder="이름" onChange={(e) => setName(e.target.value)}></Input>
+      <Input type="text" placeholder="전화번호" onChange={(e) => setPhone(e.target.value)}></Input>
+      <Input type="text" placeholder="이메일" onChange={(e) => setEmail(e.target.value)}></Input>
+      <Input type="password" placeholder="비밀번호" onChange={(e) => setPw(e.target.value)}></Input>
+      <button onClick={register}>회원가입</button>
+      <button onClick={() => {nav('/')}}>메인으로</button>
+    </Container>
+  )
+}
