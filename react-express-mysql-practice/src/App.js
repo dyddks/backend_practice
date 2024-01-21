54d2d2fc07920790f9d565d911ca966f46231b96
DESKTOP-KSUP4OT\dyddk <dyddks0115@naver.com>
2024-01-21 16:31:19 +0900

    feat: 유저정보업데이트 페이지 추가

--- react-express-mysql-practice/src/App.js
+++ react-express-mysql-practice/src/App.js
@@ -2,13 +2,25 @@ import { Route, Routes } from 'react-router-dom';
 import { MainPage } from './page/MainPage';
 import { DetailPage } from './page/DetailPage';
 import { UpdatePage } from './page/UpdatePage';
+import { WritePage } from './page/WritePage';
+import { RegisterPage } from './page/RegisterPage';
+import { UnregisterPage } from './page/UnregisterPage';
+import { LoginPage } from './page/LoginPage';
+import { BoardPage } from './page/BoardPage';
+import { UserUpdatePage } from './page/user/UserUpdatePage';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<MainPage/>}/>
+      <Route path='/board' element={<BoardPage/>}/>
       <Route path='/detail' element={<DetailPage/>}/>
       <Route path='/update' element={<UpdatePage/>}/>
+      <Route path='/write' element={<WritePage/>}/>
+      <Route path='/register' element={<RegisterPage/>}/>
+      <Route path='/login' element={<LoginPage/>}/>
+      <Route path='/unregister' element={<UnregisterPage/>}/>
+      <Route path='/user/update' element={<UserUpdatePage/>}/>
       <Route path="*" element="404 not found" />
     </Routes>
   );
